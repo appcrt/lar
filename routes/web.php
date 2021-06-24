df7438a0be0b546f1d29fcbd4ebb609911887b23
Your Name <you@example.com>
2021-06-25 01:21:05 +0300

    cat-controller

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,24 @@ Route::group(['namespace' => 'Blog','prefix' => 'blog'],function (){
     Route::resource('posts','PostController')->names('blog.posts');
 });
 
-Route::resource('rest','RestTestController')->names('restTest');
+//Route::resource('rest','RestTestController')->names('restTest');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Admin
+
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+
+Route::group($groupData,function (){
+    $methods = ['index','edit','store','update','create',];
+    Route::resource('categories','CategoryController')->only($methods)->names('blog.admin.categories');
+});
